5d0fe6eaa1b63499bab26121e98a17d49b5615fa
Quantre34 <recepozmen_67@hotmail.com>
2023-06-07 00:09:31 +0300

    After the Judgement Day

--- classes/Database.php
+++ classes/Database.php
@@ -2,7 +2,9 @@
 
 class Database
 {
-
+	// const name = 'sorupane_testDb';
+	// const User = 'sorupane_sorupanelicomuser';
+	// const Pass = '13979997622sorupaneli';
 	const name = 'testdb';
 	const User = 'root';
 	const Pass = '';
--- test.php
+++ test.php
@@ -3,14 +3,14 @@
 function test(){
 	/*
 	* Only 1 parameter is enough to Insert, Can be customized in index.php by adding the field to the necessities list
-	* If Alter, Id And is Necessary
+	* If Alter, Id AndOnly 1 parameter is Necessary
 	*/
-
+   
 	// Here Are Some Samples for Get Methods
-	$url = 'http://sorupaneli.com/devtest/?ConstructionStages/GetAll';
-	$url = 'http://sorupaneli.com/devtest/?ConstructionStages/GetAll/table/construction_stages';
-	$url = 'http://sorupaneli.com/devtest/?ConstructionStages/GetSingle/Id';
-	$url = 'http://sorupaneli.com/devtest/?ConstructionStages/GetSingle/Id/construction_stages';
+	// $url = 'http://sorupaneli.com/devtest/?ConstructionStages/GetAll';
+	// $url = 'http://sorupaneli.com/devtest/?ConstructionStages/GetAll/table/{table}';
+	// $url = 'http://sorupaneli.com/devtest/?ConstructionStages/GetSingle/{Id}';
+	// $url = 'http://sorupaneli.com/devtest/?ConstructionStages/GetSingle/Id/{table}';
 	//
 
 	$url = 'http://sorupaneli.com/devtest/';
@@ -30,16 +30,18 @@ function test(){
 
 	$ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST, false);
+    curl_setopt($ch, CURLOPT_POST, TRUE);
     curl_setopt($ch, CURLOPT_POSTFIELDS, @$data);
+    curl_setopt($ch, CURLOPT_FRESH_CONNECT, TRUE);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Cache-Control: no-cache"));
     curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
     curl_setopt($ch, CURLOPT_TIMEOUT, 10);                                                                    
 	$result = curl_exec($ch);
 	curl_close($ch);
 	return $result;
 }
-
+header('Content-Type: Application/json');
 $result = test();
 echo $result;
 
